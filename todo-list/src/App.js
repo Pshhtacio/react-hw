92e4de904ae8f1398e5c4fce89d42677d0a1747a
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-28 21:37:00 +0800

    feat: Add TodoList component in App

---
+++ todo-list/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
