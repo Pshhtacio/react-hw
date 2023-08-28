61128afbc3364843af1653d6d05941b5a916e8dd
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-28 21:37:29 +0800

    feat: create placeholder file for Todolist

---
+++ todo-list/src/components/TodoList.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import TodoListGroup from "./TodoListGroup.js";
+import TodoListGenerator from "./TodoListGenerator.js";
+
+const TodoList = () => {
+    
+    return <div>
+
+    </div>
+}
+
+export default TodoList;
