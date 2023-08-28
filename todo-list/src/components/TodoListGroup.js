02be4b7baab8bdc404a4c7cc335a9e03c9afc25e
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-28 23:37:20 +0800

    feat: add mapping in TodoListGroup component

--- todo-list/src/components/TodoListGroup.js
+++ todo-list/src/components/TodoListGroup.js
@@ -1,7 +1,14 @@
+import React from "react";
 import Todo from "./Todo";
 
-const TodoListGroup = (props) =>{
-
-}
+const TodoListGroup = (props) => {
+  return (
+    <div className="todo-list">
+      {props.todos.map((todo) => (
+        <Todo key={todo.id} text={todo.text} />
+      ))}
+    </div>
+  );
+};
 
 export default TodoListGroup;
