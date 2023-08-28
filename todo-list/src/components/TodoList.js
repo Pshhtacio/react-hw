9dbfa7eba0f004a6041b514489cd84577286b489
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-28 22:34:16 +0800

    feat: Add input functionality

--- todo-list/src/components/TodoList.js
+++ todo-list/src/components/TodoList.js
@@ -1,12 +1,28 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import TodoListGroup from "./TodoListGroup.js";
 import TodoListGenerator from "./TodoListGenerator.js";
 
 const TodoList = () => {
-    
-    return <div>
+    const [ text, setText ] = useState("");
+    const [ size, setSize ] = useState(0);
 
-    </div>
-}
+    const todoTextInput = (inputText) => {
+        setText(inputText);
+    };
+
+    const onSubmit = () => {
+        setSize(size + 1);
+    };
+
+    return (
+        <div>
+            <TodoListGroup size={size} text={text} />
+            <TodoListGenerator
+                todoTextInput={todoTextInput}
+                onSubmit={onSubmit}
+            />
+        </div>
+    );
+};
 
 export default TodoList;
--- todo-list/src/components/TodoListGenerator.js
+++ todo-list/src/components/TodoListGenerator.js
@@ -1,7 +1,27 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-const TodoListGenerator = (props) =>{
+const TodoListGenerator = (props) => {
+    const [ text, setText ] = useState("");
 
-}
+    const onInputChange = (event) => {
+        setText(event.target.value);
+    };
+
+    const onSubmit = () => {
+        props.onSubmit();
+        setText("");
+    };
+
+    return (
+        <div>
+            <input
+                type="text"
+                value={text}
+                onChange={onInputChange}
+            />
+            <button onClick={onSubmit}>Submit</button>
+        </div>
+    );
+};
 
 export default TodoListGenerator;
