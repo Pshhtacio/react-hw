a1acfa5bf65bc836cb8471780f9a3ff09a1f5c40
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-30 02:51:17 +0800

    refactor: update attribute display

--- todo-list/src/components/Todo.js
+++ todo-list/src/components/Todo.js
@@ -1,9 +1,7 @@
-import {useState} from "react";
-
 const Todo = (props) => {
     return (
         <div className="todo-item">
-            <span>{props.text}</span>
+            <span>{ props.todoText }</span>
         </div>
     );
 
