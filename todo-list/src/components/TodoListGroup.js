f48c515ccd41efa3b406d9f44bee00c950da52cf
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-30 02:47:49 +0800

    feat: add necessary attribute to cater redux migration

--- todo-list/src/components/TodoListGroup.js
+++ todo-list/src/components/TodoListGroup.js
@@ -4,9 +4,17 @@ import Todo from "./Todo";
 const TodoListGroup = (props) => {
   return (
     <div className="todo-list">
-      {props.todos.map((todo) => (
-        <Todo key={todo.id} text={todo.text} />
-      ))}
+      { props.todos.map((item, index) => {
+          return (
+            < Todo
+              key={ item.id }
+              index={ index }
+              todoText={ item.text }
+              done={ item.done }
+            />
+          );
+        }) 
+      }
     </div>
   );
 };
