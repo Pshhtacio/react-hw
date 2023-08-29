f0bd1cebafdc2825ae419e4169332c9b9e924b21
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-30 01:58:45 +0800

    refactor: remove duplicate code, utilize TodoList component

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,28 +1,12 @@
-import React, { useState } from "react";
-import TodoListGroup from "./components/TodoListGroup";
-import TodoListGenerator from "./components/TodoListGenerator";
-import "./css/App.css";
+import './css/App.css';
+import TodoList from './components/TodoList';
 
-const App = () => {
-    const [todos, setTodos] = useState([]);
-
-    const addTodo = (text) => {
-      if (text.trim() !== "") {
-          const newTodo = {
-              id: Date.now(),
-              text: text,
-          };
-          setTodos([...todos, newTodo]);
-      }
-  };
-
-    return (
-        <div className="container">
-            <h1>Todo List</h1>
-            <TodoListGroup todos={todos} />
-            <TodoListGenerator onSubmit={addTodo} />
-        </div>
-    );
-};
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+}
 
 export default App;
