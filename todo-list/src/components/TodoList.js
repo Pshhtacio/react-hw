a7b6b0fdebbe63e8ae65ca9c6d9cc9b6075f4cef
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-31 05:56:47 +0800

    refactor: modify css and css implementation

--- todo-list/src/components/TodoList.js
+++ todo-list/src/components/TodoList.js
@@ -2,6 +2,7 @@ import {useEffect} from "react";
 import {useTodos} from "../hooks/useTodos";
 import TodoGroup from "./TodoGroup.js";
 import TodoListGenerator from "./TodoListGenerator.js";
+import "../css/TodoList.css";
 
 const TodoList = () => {
   const { loadTodos } = useTodos();
