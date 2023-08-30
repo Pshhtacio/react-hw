af908c45f499f7a22e2dbec2b9a75690ebb08509
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-30 11:03:08 +0800

    feat: implement router

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -5,8 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+import { createBrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([{
+  path: "/",
+  element: <App />
+}]);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
