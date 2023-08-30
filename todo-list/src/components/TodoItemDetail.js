4ec3c1c1fab88d35db616950b645d55ad99984a1
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-31 05:56:11 +0800

    refactor: apply css

--- todo-list/src/components/TodoItemDetail.js
+++ todo-list/src/components/TodoItemDetail.js
@@ -1,24 +1,22 @@
-import {useSelector} from "react-redux";
-import {useParams} from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import "../css/TodoItemDetail.css"; // Import your CSS file
 
 const TodoItemDetail = () => {
-
-  const {id} = useParams();
+  const { id } = useParams();
   const todos = useSelector((state) => state.todo.todoList);
-  const todoItem = todos.find((item) => item.id === id)
-  
+  const todoItem = todos.find((item) => item.id === id);
+
   return (
-
-    <div className="todo-detail">
-      <h1>Todo Item Detail</h1>
-      <div>{todoItem?.id}</div>
-      <div>{todoItem?.text}</div>
+    <div className="todo-detail-container">
+      <div className="todo-detail-background"></div>
+      <div className="todo-detail">
+        <h1>Todo Item Detail</h1>
+        <div className="todo-id"><b>#</b>{todoItem?.id}</div>
+        <div className="todo-text">{todoItem?.text}</div>
+      </div>
     </div>
-
   );
-
 };
-
-
 
 export default TodoItemDetail;
