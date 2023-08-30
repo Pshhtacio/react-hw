393849164696391646bdfa383e2184b77d80a45a
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-31 00:55:46 +0800

    refactor: remove unused import and unused variable

--- todo-list/src/components/TodoList.js
+++ todo-list/src/components/TodoList.js
@@ -1,12 +1,10 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { useTodos } from "../hooks/useTodos";
+import {useEffect} from "react";
+import {useTodos} from "../hooks/useTodos";
 import TodoGroup from "./TodoGroup.js";
 import TodoListGenerator from "./TodoListGenerator.js";
 
 const TodoList = () => {
   const { loadTodos } = useTodos();
-  const dispatch = useDispatch();
   useEffect(() => {
     loadTodos();
   }, []);
