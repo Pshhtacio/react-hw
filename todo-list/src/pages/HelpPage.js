3bd7a8ee2f603c9723a29ede7d7af14af7464b25
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-31 05:54:35 +0800

    refactor: update style

--- todo-list/src/pages/HelpPage.js
+++ todo-list/src/pages/HelpPage.js
@@ -1,7 +1,15 @@
+import React from "react";
+import "../css/HelpPage.css"; 
 export default function HelpPage() {
-  return(
-    <div>
-      <h1>HELP! Call 911!</h1>
+  return (
+    <div className="help-page-container">
+      <div className="help-page-content">
+        <h1>Todo List Help</h1>
+        <p>
+          Welcome to the Todo List app! This page provides you with helpful
+          information on using the app.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
